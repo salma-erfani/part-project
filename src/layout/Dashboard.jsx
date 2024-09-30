7beb42e11a74dbd56d68e1d2d548b53164901851
Salma Erfani <salma.eztt@gmail.com>
2024-09-30 21:17:42 +0330

    [feat] add dashboard ui

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,9 +1,14 @@
 import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
 
 const Dashboard = () => {
     return (
         <div className="dashboard">
             <Navbar />
+            <main>
+                <Sidebar />
+                <div className="content">content</div>
+            </main>
         </div>
     )
 }
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,3 +1,23 @@
-const Sidebar = () => { }
+const Sidebar = () => {
+    let selected = 'home'
+
+    return (
+        <aside className="sidebar">
+            <div className={"sidebar-item " + (selected === 'home' && 'selected')}>
+                <img src="assets/images/Home.svg" alt="home icon" />
+                <p>خانه</p>
+            </div>
+            <div className={"sidebar-item " + (selected === 'stats' && 'selected')}>
+                <img src="assets/images/Statistics.svg" alt="statistics icon" />
+                <p>لیست کاربران</p>
+            </div>
+            <div className="divider" />
+            <div className="sidebar-item">
+                <img src="assets/images/Logout.svg" alt="logout icon" />
+                <p>خروج</p>
+            </div>
+        </aside>
+    )
+}
 
 export default Sidebar
