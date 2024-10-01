42be23656b016d1170387868c771aa983d486c0e
Salma Erfani <salma.eztt@gmail.com>
2024-10-01 23:28:18 +0330

    [feat] add theme toggle

--- src/components/util/SwitchInput.jsx
+++ src/components/util/SwitchInput.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react"
+
 const SwitchInput = () => {
-    let selected = 'light'
+    const [theme, setTheme] = useState('dark')
+
+    const storedTheme = localStorage.getItem('theme')
+    if (storedTheme && storedTheme !== theme) {
+        setTheme(storedTheme)
+    }
+
+    const toggleTheme = () => {
+        const nextTheme = theme === 'dark' ? 'light' : 'dark'
+        setTheme(nextTheme)
+        // update local storage
+        localStorage.setItem('theme', nextTheme)
+        // toggle body class
+        const bodyElement = document.body
+        if (nextTheme === 'light') {
+            bodyElement.classList.add('light-theme')
+        }
+        else {
+            bodyElement.classList.remove('light-theme')
+        }
+    }
 
     return (
         <div className="switch-container">
-            <div className={"switch-item " + (selected === 'light' && 'selected')}>
+            <div className={"switch-item " + (theme === 'light' && 'selected')} onClick={toggleTheme}>
                 <input type="radio" id="light" name="theme" value="light" />
                 <label htmlFor="light" hidden>light</label>
-                <img src={selected === 'light' ? "assets/images/sun-white.svg" : "assets/images/sun-gray.svg"} alt="sun" />
+                <img src={theme === 'light' ? "assets/images/sun-white.svg" : "assets/images/sun-gray.svg"} alt="sun" />
             </div>
-            <div className={"switch-item " + (selected === 'dark' && 'selected')}>
+            <div className={"switch-item " + (theme === 'dark' && 'selected')} onClick={toggleTheme}>
                 <input type="radio" id="dark" name="theme" value="dark" />
                 <label htmlFor="dark" hidden>dark</label>
-                <img src={selected === 'dark' ? "assets/images/moon-white.svg" : "assets/images/moon-gray.svg"} alt="moon" />
+                <img src={theme === 'dark' ? "assets/images/moon-white.svg" : "assets/images/moon-gray.svg"} alt="moon" />
             </div>
         </div>
     )
